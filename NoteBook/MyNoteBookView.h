ee43b59239bed0799c853f750b0b27ee7ade71e0
reference <imti_bandianhong@126.com>
2011-08-19 12:27:52 +0800

    add my notebook view
    from UIView

---
+++ NoteBook/MyNoteBookView.h
@@ -0,0 +1,14 @@
+//
+//  MyNoteBookView.h
+//  NoteBook
+//
+//  Created by admin on 11-8-19.
+//  Copyright (c) 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MyNoteBookView : UIView
+
+@property (nonatomic,retain)UILabel *label;
+@end
